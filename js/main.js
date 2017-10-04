cfae93bd11ec132c29565b26c65a19fad9c7485e
Phani Yellela <phani1695@gmail.com>
2017-10-04 20:45:37 +0530

    check once comment changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log('Welcome to Varnam');
+/*------Tabs javascript------------------*/
+
+var openTab=function (tabId) {
+	var tab = document.querySelectorAll(".content");
+	for (var i = 0; i<= tab.length - 1; i++) {
+		tab[i].style.display="none";
+	}
+	document.getElementById(tabId).style.display = "block"
+	
+}
+/*-------------Popup Script----------------*/
+function showPop(){
+    document.getElementById("show").style.visibility="visible";
+    document.getElementById("showpop").style.visibility="visible";
+}
+function hidePop(){
+    document.getElementById("show").style.visibility="hidden";
+    document.getElementById("showpop").style.visibility="hidden";
+}
