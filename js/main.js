3a97833fea1f4d257432e4c6689dc503d4230107
Phani Yellela <phani1695@gmail.com>
2017-10-05 21:41:18 +0530

    modified-List

--- js/main.js
+++ js/main.js
@@ -17,3 +17,38 @@ function hidePop(){
     document.getElementById("show").style.visibility="hidden";
     document.getElementById("showpop").style.visibility="hidden";
 }
+
+/*----------------Accordion----------------*/
+
+
+/*-------------- coursoel -------*/
+
+
+
+
+/*---------------Model Dialouge--------------*/
+
+function showDialogue(){
+    document.getElementById("showModel").style.visibility="visible";
+    document.getElementById("showDialogue").style.visibility="visible";
+}
+function hideDialogue (){
+    document.getElementById("showModel").style.visibility="hidden";
+    document.getElementById("showDialogue").style.visibility="hidden";
+}
+/*-----------Toster-----------*/
+function closeAlert(statusText){
+			if(statusText=='default'){
+				document.getElementById("defaultAlert").style.display = "none";
+			}else if(statusText=='info'){
+				document.getElementById("infoAlert").style.display = "none";
+			}else if(statusText=='primary'){
+				document.getElementById("primaryAlert").style.display = "none";
+			}else if(statusText=='success'){
+				document.getElementById("successAlert").style.display = "none";
+			}else if(statusText=='warning'){
+				document.getElementById("warningAlert").style.display = "none";
+			} else{
+				document.getElementById("dangerAlert").style.display = "none";
+			}
+}
